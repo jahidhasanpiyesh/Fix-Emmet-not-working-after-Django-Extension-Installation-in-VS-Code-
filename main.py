b6c7e8dc4c120f8047724627322c00d08d9b3838
jahidhasanpiyesh <jahidhasanpiyesh@gmail.com>
2023-11-10 23:09:00 +0600

    Problem face and try to fix this problem and solve it .

---
+++ main.py
@@ -0,0 +1,12 @@
+# Fix Emmet not working after Django Extension Installation in VS Code
+# simple way this problem solve ----!
+
+# just open to vc-code and settings.json.
+# Then at the end of the line just put the following code with a comma after the parentheses above.
+
+
+"emmet.includeLanguages": {
+    "django-html": "html",
+}
+
+#  ----------------- Solve This Problem ------------------
